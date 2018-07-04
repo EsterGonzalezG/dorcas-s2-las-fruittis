fcde16968def9e2c209fa893b712933cbe5ef82e
Fanyfa <estefania.salgado@gmail.com>
2018-07-04 22:46:11 +0200

    js design

---
+++ js/design.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var cardContainer = document.querySelector('.viewfinder');
+var colorContainer = document.querySelectorAll('.dropdown__colorsgroups');
+var radioRed = document.querySelector('.radio--red');
+var radioGrey = document.querySelector('.radio--grey');
+
+function init() {
+  for(var i = 0; i < colorContainer.length; i++) {
+    colorContainer[i].addEventListener('click', setStyles);
+  }
+}
+
+function setStyles() {
+  if (radioRed.checked) {
+    cardContainer.className = 'viewfinder viewfinder--red' /*className remplaza el valor de la clase actual por esta*/
+  } else if (radioGrey.checked) {
+    cardContainer.className = 'viewfinder viewfinder--grey'
+  } else {
+    cardContainer.className = 'viewfinder viewfinder--blue'
+  }
+}
+
+
+init();
