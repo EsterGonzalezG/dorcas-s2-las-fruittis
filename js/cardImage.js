258d869309cc6bc4bebb5fe0abf6a6bb2d3c9dc3
EsterGonzalezG <estergonzalezgalindo@gmail.com>
2018-07-06 10:50:51 +0200

    fotoe en miniatura

--- js/cardImage.js
+++ js/cardImage.js
@@ -3,7 +3,7 @@ var fr = new FileReader();
 var buttonImage=document.querySelector('.fill__input--addimgbutton');
 var inputHidden=document.querySelector('#img-selector');
 var imageCard= document.querySelector('.viewfinder__photo');
-
+var miniImage=document.querySelector('.fill__input--miniimg');
 function getImage(event){
   var myFile = event.currentTarget.files[0];
   fr.addEventListener('load', writeImage);
@@ -11,6 +11,7 @@ function getImage(event){
 }
 function writeImage() {
   imageCard.style.backgroundImage= 'url('+ fr.result+')';
+  miniImage.style.backgroundImage= 'url('+ fr.result+')';
 }
 function fakeFileClick() {
   inputHidden.click();
