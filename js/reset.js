9ce03a976fe1020348fb06a315b5089d2cce11f2
lauradml <laura-domingo.82@gmail.com>
2018-07-09 21:37:25 +0200

    función para borrrar los formularios

---
+++ js/reset.js
@@ -0,0 +1,10 @@
+'use strict';
+var buttonReset= document.querySelector('.viewfinder__button-reset');
+function resetForm (){
+  document.getElementById("fill__form--itemscontainer").reset();
+}
+function resetCard(){
+  document.getElementAll(".viewfinder__card-titlebox").reset();
+}
+
+buttonReset.addEventListener('click', resetForm, resetCard);
