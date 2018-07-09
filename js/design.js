f80c28610b83df2a83f110f9e79a35bb28ba58b6
Fanyfa <estefania.salgado@gmail.com>
2018-07-09 18:30:09 +0200

    cambio de fuentes

--- js/design.js
+++ js/design.js
@@ -1,18 +1,24 @@
 'use strict';
 
 var cardContainer = document.querySelector('.viewfinder');
-var colorContainer = document.querySelectorAll('.dropdown__colorsgroups');
+var radiosGroup = document.querySelectorAll('.radiosGroup');
 var radioRed = document.querySelector('.radio--red');
 var radioGrey = document.querySelector('.radio--grey');
 var radios = document.querySelectorAll('.radio');
+var radioUbuntu = document.querySelector('.radio--ubuntu');
+var radioComic = document.querySelector('.radio--comic');
+var radioMontserrat = document.querySelector('.radio--montserrat');
+
+
 
 function init() {
-  for(var i = 0; i < colorContainer.length; i++) {
+  for(var i = 0; i < radiosGroup.length; i++) {
     radios[i].addEventListener('click', setStyles);
   }
 }
 
 function setStyles() {
+console.log('hi');
   resetColor();
   if (radioRed.checked) {
     cardContainer.classList.add('viewfinder--red');
@@ -21,10 +27,17 @@ function setStyles() {
   } else {
     cardContainer.classList.add('viewfinder--blue');
   }
+  if (radioComic.checked) {
+    cardContainer.classList.add('comic');
+  } else if (radioMontserrat.checked) {
+    cardContainer.classList.add('montserrat');
+  } else {
+    cardContainer.classList.add('ubuntu');
+  }
 }
 
 function resetColor() {
-  cardContainer.classList.remove('viewfinder--red', 'viewfinder--grey', 'viewfinder--blue');
+  cardContainer.classList.remove('viewfinder--red', 'viewfinder--grey', 'viewfinder--blue', 'comic', 'montserrat', 'ubuntu');
 }
 
 init();
