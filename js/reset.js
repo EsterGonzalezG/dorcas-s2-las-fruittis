bc32ee94b4969032496d183fc3743b66588bf6e5
lauradml <laura-domingo.82@gmail.com>
2018-07-10 11:31:03 +0200

    conflicto map

--- js/reset.js
+++ js/reset.js
@@ -1,10 +1,10 @@
 'use strict';
 var buttonReset= document.querySelector('.viewfinder__button-reset');
 function resetForm (){
-  document.getElementById("fill__form--itemscontainer").reset();
+  document.getElementById('.fill__form--itemscontainer').reset();
 }
 function resetCard(){
-  document.getElementAll(".viewfinder__card-titlebox").reset();
+  document.getElementAll('.viewfinder__card-titlebox').reset();
 }
 
 buttonReset.addEventListener('click', resetForm, resetCard);
