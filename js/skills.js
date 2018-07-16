748f17e0a72bd2e121bad1e8bca19a76c23dd424
Marta Ocaña <marta.o.padilla@gmail.com>
2018-07-16 16:50:01 +0200

    añadidos comentarios a skills.js

--- js/skills.js
+++ js/skills.js
@@ -5,12 +5,16 @@ var removeAbilityButton = document.querySelectorAll('.abilitiesbutton--minusjs')
 var fillAbilityBox = document.querySelectorAll('.fill__ability--js');
 var abilitiesDropdown = document.querySelectorAll('.abilitiesdropdown--js');
 
+//Esta función crea los elementos "option" que van dentro del select
+
 function createOptionForSelect(text) {
   var option = document.createElement('option');
   var optionText = document.createTextNode(text);
   option.appendChild(optionText);
   return option;
 }
+
+//Esta función llama al API y añade las habilidades a cada option
 
 function callAbilitiesAPI() {
   var url = 'https://raw.githubusercontent.com/Adalab/dorcas-s2-proyecto-data/master/skills.json';
@@ -29,6 +33,8 @@ function callAbilitiesAPI() {
     });
 }
 
+//Esta función hace que se añadan la segunda y tercera caja para seleccionar las skills
+
 function addAbility() {
   var hiddenAbilityBox = document.querySelectorAll('.fill__ability--jshidden');
 
@@ -39,15 +45,21 @@ function addAbility() {
   }
 }
 
+//Esta función permite eliminar la segunda y tercera caja de skills
+
 function removeAbility(event) {
   var parentBox = event.currentTarget.parentElement;
   parentBox.classList.add('fill__ability--jshidden');
 }
 
+//Las llamadas a las funciones anteriores mediante click
+
 addAbilityButton.addEventListener('click', addAbility);
 removeAbilityButton[0].addEventListener('click', removeAbility);
 removeAbilityButton[1].addEventListener('click', removeAbility);
 callAbilitiesAPI();
+
+//De aquí hacia abajo va la función que hace que el valor seleccionado en los select con skills de "rellena" se transfiera al visor de la card, pero no funciona del todo bien...
 
 function transferabilitiesToCard () {
   var abilitiesFromSelect;
