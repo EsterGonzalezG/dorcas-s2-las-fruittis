bfda25159477993aed8b5e7012fdde218f280ef9
АНЯ <annya.branco@gmail.com>
2018-07-18 12:05:46 +0200

    Desactivado localStorage

--- js/cardImage.js
+++ js/cardImage.js
@@ -1,24 +1,25 @@
 'use strict';
 
 var fr = new FileReader();
-var buttonImage=document.querySelector('.fill__input--addimgbutton');
-var inputHidden=document.querySelector('#img-selector');
-var imageCard= document.querySelector('.viewfinder__photo');
-var miniImage=document.querySelector('.fill__input--miniimg');
+var buttonImage = document.querySelector('.fill__input--addimgbutton');
+var inputHidden = document.querySelector('#img-selector');
+var imageCard = document.querySelector('.viewfinder__photo');
+var miniImage = document.querySelector('.fill__input--miniimg');
 var urlPhoto;
 var miniPhoto;
-function getImage(event){
+
+function getImage(event) {
   var myFile = event.currentTarget.files[0];
   fr.addEventListener('load', writeImage);
   fr.readAsDataURL(myFile);
 }
 
 function writeImage() {
-  urlPhoto='url('+ fr.result+')';
-  miniPhoto='url('+ fr.result+')';
-  imageCard.style.backgroundImage= 'url('+ fr.result+')';
-  miniImage.style.backgroundImage= 'url('+ fr.result+')';
-  guardarCardLocal();
+  urlPhoto = 'url(' + fr.result + ')';
+  miniPhoto = 'url(' + fr.result + ')';
+  imageCard.style.backgroundImage = 'url(' + fr.result + ')';
+  miniImage.style.backgroundImage = 'url(' + fr.result + ')';
+  // guardarCardLocal();
 }
 
 function fakeFileClick() {
