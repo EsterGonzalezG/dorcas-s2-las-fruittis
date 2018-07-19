005dc5bffd025508013c7839f223ee9798ab3f21
SandraCGarcia <sandra.gcanete@gmail.com>
2018-07-19 12:15:02 +0200

    building collapsibles

--- js/colapsable.js
+++ js/colapsable.js
@@ -1,28 +1,23 @@
 'use strict';
 
-var i;
-var j;
-var collapsible=document.querySelectorAll('.collapsible');
-var collapsibleTriggers=document.querySelectorAll('.collapsible__label');
+const collapsible = document.querySelectorAll('.collapsible');
+const collapsibleTriggers = document.querySelectorAll('.collapsible__label');
 
-function updateCollapsibles(e) {
-  var parent = e.currentTarget.parentElement;
+for (const trigger of collapsibleTriggers) {
+  trigger.addEventListener('click', (evento) => {
+    const parent = evento.currentTarget.parentElement;
 
-  if (parent.classList.contains('collapsible--visible')) {
-    parent.classList.remove('collapsible--visible');
-  } else {
-    closeAllCollapsibles();
-    parent.classList.add('collapsible--visible');
+    if (parent.classList.contains('collapsible--visible')) {
+      parent.classList.remove('collapsible--visible');
+    } else {
+      closeAllCollapsibles();
+      parent.classList.add('collapsible--visible');
+    }
+  });
+}
 
+const closeAllCollapsibles = () =>{
+  for (const close of collapsible){
+    close.parentElement.classList.remove('collapsible--visible');
   }
-}
-
-function closeAllCollapsibles() {
-  for (i = 0; i < collapsible.length; i++) {
-    collapsible[i].classList.remove('collapsible--visible');
-  }
-}
-
-for (j = 0; j < collapsibleTriggers.length; j++) {
-  collapsibleTriggers[j].addEventListener('click', updateCollapsibles);
-}
+};
