138fd98528d8095bb7ab78fe9b62d5fa20ff2a7f
АНЯ <annya.branco@gmail.com>
2018-07-20 11:30:59 +0200

    Colapsables terminados ES6

--- js/colapsable.js
+++ js/colapsable.js
@@ -3,9 +3,15 @@
 const collapsible = document.querySelectorAll('.collapsible');
 const collapsibleTriggers = document.querySelectorAll('.collapsible__label');
 
+const closeAllCollapsibles = () => {
+  for (const close of collapsible) {
+    close.classList.remove('collapsible--visible');
+  }
+};
+
 for (const trigger of collapsibleTriggers) {
   trigger.addEventListener('click', (evento) => {
-    const parent = evento.currentTarget.parentElement;
+    const parent = evento.currentTarget.parentElement.parentElement;
 
     if (parent.classList.contains('collapsible--visible')) {
       parent.classList.remove('collapsible--visible');
@@ -15,9 +21,3 @@ for (const trigger of collapsibleTriggers) {
     }
   });
 }
-
-const closeAllCollapsibles = () =>{
-  for (const close of collapsible){
-    close.parentElement.classList.remove('collapsible--visible');
-  }
-};
