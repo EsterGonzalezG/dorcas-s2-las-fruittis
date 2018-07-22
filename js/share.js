972ec817896c3b02084de6f74b58243cf11113dd
Imoa1991 <ordonez.im@gmail.com>
2018-07-22 20:25:24 +0200

    Pasado a ES6 share

--- js/share.js
+++ js/share.js
@@ -14,6 +14,52 @@ const linkedinForm = document.querySelector('.fill__input-linkedin');
 const githubForm = document.querySelector('.fill__input-github');
 const photoForm = document.querySelector('.action__hiddenField');
 
+const getPaletteValue = () => {
+  const palettes = document.querySelectorAll('input[name="palette"]');
+  let value;
+  for (const element of palettes) {
+    if (element.checked) {
+      value = element.value;
+    }
+  }
+  return parseInt(value);
+};
+
+const getTypographyValue = () => {
+  let value;
+  let typography = document.querySelectorAll('input[name="typography"]');
+  for (const element of typography) {
+    if (element.checked) {
+      value = element.value;
+    }
+  }
+  return parseInt(value);
+};
+
+const getSkillsValue = () => {
+  let value = [];
+  let skills = document.querySelectorAll('.fill__ability--js:not(.fill__ability--jshidden) select');
+  for ( const element of skills) {
+    value.push(element.value);
+  }
+  return value;
+};
+
+const setCardUrl = (url) => {
+  sharesUrl.innerHTML = url;
+  sharesUrl.href = url;
+  shares.classList.remove('hidden');
+};
+
+const fillPhotoFileReader = (event) => {
+  let myFile = event.currentTarget.files[0];
+  photoFileReader.readAsDataURL(myFile);
+};
+
+const shareUrlTwitter = (url) => {
+  buttonTwitter.href = 'https://twitter.com/intent/tweet?url=' + url + '&text=Acabo de crear mi tarjeta&hashtags=WomenInTech';
+};
+
 const createUrlCard = () => {
   loader.classList.remove('hidden');
   shares.classList.add('hidden');
@@ -39,67 +85,10 @@ const createUrlCard = () => {
     .then((response) => response.json())
     .then((json) => {
       loader.classList.add('hidden');
-console.log(json);
       setCardUrl(json.cardURL);
       shareUrlTwitter(json.cardURL);
     });
 };
-const getPaletteValue = () => {
-  const palettes = document.querySelectorAll('input[name="palette"]');
-  let value;
-  for (const element of palettes) {
-    if (element.checked) {
-      value = element.value;
-    }
-  }
-  return parseInt(value);
-};
-
-// function getPaletteValue() {
-//   var value;
-//   var palettes = document.querySelectorAll('input[name="palette"]');
-//   for (var i = 0; i < palettes.length; i++) {
-//     if (palettes[i].checked) {
-//       value = palettes[i].value;
-//     }
-//   }
-//   return parseInt(value);
-// }
-
-function getTypographyValue() {
-  var value;
-  var typography = document.querySelectorAll('input[name="typography"]');
-  for (var i = 0; i < typography.length; i++) {
-    if (typography[i].checked) {
-      value = typography[i].value;
-    }
-  }
-  return parseInt(value);
-}
-
-function getSkillsValue() {
-  var value = [];
-  var skills = document.querySelectorAll('.fill__ability--js:not(.fill__ability--jshidden) select');
-  for (var i = 0; i < skills.length; i++) {
-    value.push(skills[i].value);
-  }
-  return value;
-}
-
-function setCardUrl(url) {
-  sharesUrl.innerHTML = url;
-  sharesUrl.href = url;
-  shares.classList.remove('hidden');
-}
-
-function fillPhotoFileReader(event) {
-  var myFile = event.currentTarget.files[0];
-  photoFileReader.readAsDataURL(myFile);
-}
-
-function shareUrlTwitter(url) {
-  buttonTwitter.href = 'https://twitter.com/intent/tweet?url=' + url + '&text=Acabo de crear mi tarjeta&hashtags=WomenInTech';
-}
 
 photoForm.addEventListener('change', fillPhotoFileReader);
 buttonCreateCard.addEventListener('click', createUrlCard);
